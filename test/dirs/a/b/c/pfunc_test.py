f13ddb55a44da29d38849668942f41fd1822eda5
gitpillow <gitpillow>
2020-04-09 19:13:19 +0800

    add wrap function return struct and map test

--- test/dirs/a/b/c/pfunc_test.py
+++ test/dirs/a/b/c/pfunc_test.py
@@ -2,14 +2,29 @@
 def func1():
     print "this is func1"
 
+
 def func2(a, b):
     return a + b
+
 
 def func3(a, b, c):
     return a['name'] + " and " + b['name'] + " and " + c['name']
 
+
 def func4(a, b):
     return a / b
 
+
 def func5(a, b):
     return float(a) / float(b)
+
+
+def func_return_struct(name, age, hobby1, hobby2):
+    return {
+        "name": name,
+        "age": age,
+        "hobby": [
+            hobby1,
+            hobby2
+        ]
+    }
--- test/pfunc_wrap_test.go
+++ test/pfunc_wrap_test.go
@@ -6,6 +6,12 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name  string   `json:"name"`
+	Age   int      `json:"age"`
+	Hobby []string `json:"hobby"`
+}
+
 func divide(a int, b int) (int, error) {
 	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "func4").
 		Params(a, b).
@@ -13,6 +19,28 @@ func divide(a int, b int) (int, error) {
 		Do()
 
 	return i.(int), err
+}
+
+func GetPerson(name string, age int, hobby1 string, hobby2 string) (Person, error) {
+	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "func_return_struct").
+		Params(name, age, hobby1, hobby2).
+		Return(Person{
+			Name:  "Nobody",
+			Age:   0,
+			Hobby: []string{},
+		}).
+		Do()
+
+	return i.(Person), err
+}
+
+func GetPersonMap(name string, age int, hobby1 string, hobby2 string) (map[string]interface{}, error) {
+	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "func_return_struct").
+		Params(name, age, hobby1, hobby2).
+		Return(map[string]interface{}{}).
+		Do()
+
+	return i.(map[string]interface{}), err
 }
 
 func TestWrapFunction(t *testing.T) {
@@ -23,4 +51,16 @@ func TestWrapFunction(t *testing.T) {
 	i, e = divide(6, 0)
 	fmt.Println(i)
 	fmt.Println(e)
+}
+
+func TestWrapFunctionReturnStruct(t *testing.T) {
+	person, err := GetPerson("Tom", 33, "Football", "Shopping")
+	fmt.Println(person)
+	fmt.Println(err)
+}
+
+func TestWrapFunctionReturnMap(t *testing.T) {
+	personMap, err := GetPersonMap("Tom", 33, "Football", "Shopping")
+	fmt.Println(personMap)
+	fmt.Println(err)
 }
