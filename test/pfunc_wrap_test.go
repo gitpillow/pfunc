fefafef0f3de45f01cb59a5173fc4a821f405eb3
gitpillow <gitpillow>
2020-04-14 15:10:10 +0800

    edit func wrap test

--- test/pfunc_wrap_test.go
+++ test/pfunc_wrap_test.go
@@ -3,6 +3,7 @@ package test
 import (
 	"fmt"
 	"github.com/gitpillow/pfunc"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
@@ -12,8 +13,9 @@ type Person struct {
 	Hobby []string `json:"hobby"`
 }
 
+// wrap divide function in python script
 func divide(a int, b int) (int, error) {
-	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "func4").
+	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "divide").
 		Params(a, b).
 		Return(int(0)).
 		Do()
@@ -21,6 +23,7 @@ func divide(a int, b int) (int, error) {
 	return i.(int), err
 }
 
+/// wrap func_return_struct function in python script to assemble a struct
 func GetPerson(name string, age int, hobby1 string, hobby2 string) (Person, error) {
 	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "func_return_struct").
 		Params(name, age, hobby1, hobby2).
@@ -34,6 +37,7 @@ func GetPerson(name string, age int, hobby1 string, hobby2 string) (Person, erro
 	return i.(Person), err
 }
 
+/// wrap func_return_struct function in python script to assemble a map with string keys
 func GetPersonMap(name string, age int, hobby1 string, hobby2 string) (map[string]interface{}, error) {
 	i, err := pfunc.Func("dirs/a/b/c/pfunc_test.py", "func_return_struct").
 		Params(name, age, hobby1, hobby2).
@@ -47,20 +51,46 @@ func TestWrapFunction(t *testing.T) {
 	i, e := divide(6, 3)
 	fmt.Println(i)
 	fmt.Println(e)
+	assert.Equal(t, 2, i)
+	assert.Nil(t, e)
 
 	i, e = divide(6, 0)
 	fmt.Println(i)
 	fmt.Println(e)
+	assert.Equal(t, 0, i)
+	assert.Contains(t, e.Error(), "by zero")
 }
 
 func TestWrapFunctionReturnStruct(t *testing.T) {
 	person, err := GetPerson("Tom", 33, "Football", "Shopping")
 	fmt.Println(person)
 	fmt.Println(err)
+
+	assert.Equal(t,
+		Person{
+			Name: "Tom",
+			Age:  33,
+			Hobby: []string{
+				"Football",
+				"Shopping",
+			}},
+		person)
+	assert.Nil(t, nil)
 }
 
 func TestWrapFunctionReturnMap(t *testing.T) {
 	personMap, err := GetPersonMap("Tom", 33, "Football", "Shopping")
 	fmt.Println(personMap)
 	fmt.Println(err)
+
+	assert.Equal(t,
+		map[string]interface{}{
+			"name": "Tom",
+			"age":  float64(33),
+			"hobby": []interface{}{
+				"Football",
+				"Shopping",
+			}},
+		personMap)
+	assert.Nil(t, nil)
 }
